19eaf3ab9f40c2b40c00297de58ef4f15d9ece07
jiangyue95 <yuejiang811@gmail.com>
2026-02-25 12:49:58 +0000

    add solution of LeetCode 226. Invert Binary Tree using 分解问题的思想

--- src/invertbinarytree/Solution.java
+++ src/invertbinarytree/Solution.java
@@ -4,6 +4,7 @@ package invertbinarytree;
 import basicdatastructure.TreeNode;
 
 public class Solution {
+    // 使用遍历的思维模式
     public TreeNode invertTree(TreeNode root) {
         traverse(root);
         return root;
@@ -20,5 +21,20 @@ public class Solution {
 
         traverse(root.left);
         traverse(root.right);
+    }
+
+    // 使用“分解问题”的思维模式
+    public TreeNode invertTree1(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+
+        TreeNode left = invertTree1(root.left);
+        TreeNode right = invertTree1(root.right);
+
+        root.left = right;
+        root.right = left;
+
+        return root;
     }
 }
