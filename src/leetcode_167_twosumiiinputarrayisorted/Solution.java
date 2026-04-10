7f2f059aeb728cf73d8f5066145a1b5a33fe7dbf
jiangyue95 <yuejiang811@gmail.com>
2026-04-10 09:24:12 +0100

    Rename leetcode 167 and ad some comments

---
+++ src/leetcode_167_twosumiiinputarrayisorted/Solution.java
@@ -0,0 +1,27 @@
+// LeetCode 167. Two Sum II - Input Array is Sorted
+// Tag: Two Pointers
+package leetcode_167_twosumiiinputarrayisorted;
+
+// The array is ordered, hence we can use two pointers.
+public class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        // left and right pointer move towards each other
+        int left = 0;
+        int right = numbers.length - 1;
+
+        while (left < right) {
+            int sum = numbers[left] + numbers[right];
+
+            if (sum == target) {
+                // Attention: index begains with 1
+                return new int[] {left + 1, right + 1};
+            } else if (sum < target) {
+                left++;
+            } else if (sum > target) {
+                left--;
+            }
+        }
+        
+        return new int[] {-1, -1};
+    }
+}
