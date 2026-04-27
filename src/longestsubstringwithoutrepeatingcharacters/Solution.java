68bee7e20aae3684e320e920dfff0275467bf5cc
jiangyue95 <yuejiang811@gmail.com>
2026-04-27 15:11:37 +0100

    Add some comments of the solution of LeetCode 3. Longest Substring Without Repeating Characters

--- src/longestsubstringwithoutrepeatingcharacters/Solution.java
+++ src/longestsubstringwithoutrepeatingcharacters/Solution.java
@@ -1,33 +1,45 @@
 // LeetCode 3. Longest Substring Without Repeating Characters
+// Tags: Substring, Sliding Window
 package longestsubstringwithoutrepeatingcharacters;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class Solution {
+
+    /**
+     * Find the length of the longest substring without duplicate characters
+     * @param s a string
+     * @return the length of the longest substring
+     */
     public int lengthOfLongestSubstring(String s) {
-        // 用 HashMap 充当窗口，记录字符出现次数
+        // Use HashMap as the window store the times that character showes up
         Map<Character, Integer> window = new HashMap<>();
 
+        // The boundary of the window, initialized as 0
         int left = 0;
         int right = 0;
-        int res = 0; // 记录结果
+
+        // Store the result
+        int res = 0;
 
         while (right < s.length()) {
             char c = s.charAt(right);
+            // Right boundary moves to right
             right++;
-            // 窗口内数据的一系列更新
+            // Update the data in the window
             window.put(c, window.getOrDefault(c, 0) + 1);
 
-            // 判断左侧窗口是否需要收缩
+            // Check if the left boundary need to be shrinked
             while (window.get(c) > 1) {
                 char d = s.charAt(left);
+                // Left boundary moves to right
                 left++;
-                // 窗口内数据的一系列更新
+                // Update the data in the window
                 window.put(d, window.get(d) - 1);
             }
 
-            // 在这里更新答案
+            // Update the result
             res = Math.max(res, right - left);
         }
 
