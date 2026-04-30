0b9b4786d9b87fec35b60b811c44b8acd28eb60a
jiangyue95 <yuejiang811@gmail.com>
2026-04-30 17:12:43 +0100

    Add some Chinese comments on the solution of LeetCode 857. Minimum Cost to Hire K Workers

--- src/leetcode_857_minimumcosttohirekworkers/Solution.java
+++ src/leetcode_857_minimumcosttohirekworkers/Solution.java
@@ -7,30 +7,38 @@ import java.util.PriorityQueue;
 
 public class Solution {
     public double mincostToHireWorkers(int[] quality, int[] wage, int k) {
+        // 1.创建工人数组，按比值排序
         // Create the workers array, ordered by the ratio of wage / quality
         int n = quality.length;
         Worker[] workers = new Worker[n];
         for (int i = 0; i < n; i++) {
             workers[i] = new Worker(quality[i], wage[i]);
         }
+        // 写了一个自定义的 比较算法
         Arrays.sort(workers, (a, b) -> Double.compare(a.ratio, b.ratio));
 
+        // 2. 大根堆（Max heap）存 quality （用负数模拟），long 防溢出
         // Create a max heap stores the quality
+        // 自定义比较器
         PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b - a);
         long totalQuality = 0;
         double ans = Double.MAX_VALUE;
 
+        // 3. 枚举每个工人作为比值上限
         // Enumerate each ratio of the worker
         for (Worker w : workers) {
+            // 加当前工人的 quality
             // Add cureent worker's ratio
             pq.offer(w.quality);
             totalQuality += w.quality;
 
+            // 堆超 k 个，移除质量最大的（保持质量最小）
             // If the count exceed the k, remove the max
             if (pq.size() > k) {
                 totalQuality -= pq.poll();
             }
 
+            // 堆正好 k 个时，计算成本
             // When the count is k, calculate the cost
             if (pq.size() == k) {
                 double cost = totalQuality * w.ratio;
@@ -43,10 +51,11 @@ public class Solution {
         return ans;
     }
 
+    // 工人类
     // Worker class
     static class Worker {
         int quality;
-        double ratio;
+        double ratio; // wage / quality
 
         Worker(int q, int wage) {
             this.quality = q;
